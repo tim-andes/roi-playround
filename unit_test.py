fa48a0f849761953225bb7ea597d896a02bdfc79
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-01-23 12:18:30 -0500

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,11 @@
+import roi_playground
+
+# Adjusted rate / real_rate
+@pytest.mark.parametrize("growth_rate, inflation_rate, expected",
+                         [(.05, .03, .01942),
+                          (.1, .03, 0.06796),
+                          (0, 0, 0),
+                          (1, .5, 0.33333)
+])
+def test_real_rate(growth_rate, inflation_rate, expected):
+    assert roi_playground.real_rate(growth_rate, inflation_rate) == expected
