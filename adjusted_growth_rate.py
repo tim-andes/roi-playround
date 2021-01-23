066766fc950ee40740b9c1ad565429691e71157a
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-01-23 12:16:47 -0500

    Create adjusted_growth_rate.py

---
+++ adjusted_growth_rate.py
@@ -0,0 +1,12 @@
+def real_rate(growth_rate, inflation_rate):
+	"""
+	
+	:param growth_rate: Growth rate of investment
+	:param inflation_rate: Rate of inflation
+	:return: Adjusted growth rate after inflation
+	"""
+	real_rate = ((1 + growth_rate) / (1 + inflation_rate)) - 1
+	return round(real_rate, 5)
+  
+  if __name__ == "__main__":
+	real_rate(growth_rate, inflation_rate)
