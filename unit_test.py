ed468a17f289d8d05581c4f2ab4f4d41fada1c72
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-01-23 12:20:50 -0500

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -5,6 +5,7 @@ import roi_playground
                          [(.05, .03, .01942),
                           (.1, .03, 0.06796),
                           (0, 0, 0),
-                          (1, .5, 0.33333)])
+                          (1, .5, 0.33333),
+                          (0, 0, 0)])
 def test_real_rate(growth_rate, inflation_rate, expected):
     assert roi_playground.real_rate(growth_rate, inflation_rate) == expected
