e94afb88512a47a71a184f053fb0b53bc66c830a
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-01-23 13:16:22 -0500

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -10,3 +10,15 @@ import roi_no_contributions
 def test_real_rate(growth_rate, inflation_rate, expected):
     assert roi_playground.real_rate(growth_rate, inflation_rate) == expected  
 
+# ROI, no contributions
+@pytest.mark.parametrize("starting_principal, years_invested, compound_frequency,"
+                        "real_rate, expected",
+                        [
+                        (0, 0, 0, 0, 0),
+                        (10000, 10, 1, .01942, 12121),
+                        (220000, 25, 1, .06796, 1138406)
+])
+def test_roi(starting_principal, years_invested, compound_frequency, real_rate,
+             expected):
+    assert roi_playground.roi(starting_principal, years_invested,
+                              compound_frequency, real_rate) == expected
