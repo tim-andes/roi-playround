e2082078e1f9f242af235e9f2ef7015b7d872a64
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-01-23 13:15:24 -0500

    Create roi_no_contributions.py

---
+++ roi_no_contributions.py
@@ -0,0 +1,27 @@
+import adjusted_growth_rate
+
+def roi(starting_principal, years_invested, compound_frequency, real_rate):
+	"""
+	Calculate return on investment, excluding monthly contributions.
+
+	:param starting_principal: Starting capital
+	:param years_invested: Number of years monies will be invested
+	:param compound_frequency: Frequency that interest compounds (1 for anually)
+	:param real_rate: Rate of growth adjusted for inflation
+	:return: Final balance after year over year growth
+	"""
+	if compound_frequency == 0: # handle 0 division error
+		compound_frequency = 1
+
+	final_balance = (1 + real_rate / compound_frequency)
+	final_balance **= (years_invested * compound_frequency)
+	final_balance *= starting_principal
+	final_balance = round(final_balance)
+
+	print(final_balance)
+
+	return final_balance
+  
+  if __name__ == "__main__":
+    real_rate = adjusted_growth_rate.real_rate(growth_rate, inflation_rate)
+    roi(starting_principal, years_invested, compound_frequency, real_rate)
